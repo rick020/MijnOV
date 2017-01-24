9ba20421a68e0c6eecbb64307ec122d5df0ff450
Rick Bruins <rickbruins020@gmail.com>
2017-01-24 16:49:41 +0100

    Initial Commit

---
+++ MijnOV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MijnOV
+//
+//  Created by Rick Bruins on 24/01/2017.
+//  Copyright © 2017 Mprog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
